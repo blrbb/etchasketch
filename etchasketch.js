9b2e415e008e647b8785295230326ed499374bf7
Brandon <17446452+blrbb@users.noreply.github.com>
2023-09-24 23:39:39 -0700

    Finished etchasketch project - added hover event listener, change cell color, remove the borders, reset button, dynamically set size of grid based on user input.

--- etchasketch.js
+++ etchasketch.js
@@ -1,15 +1,32 @@
 "use strict";
 
 let gameDiv = document.getElementById("game-container");
+let numCellsPerEdge = 64;
+resetGame();
 
-for (let i = 0; i < 16; i++) {
-    let gridRow = document.createElement("div");
-    gridRow.classList.add("grid-row")
-    for (let i = 0; i < 16; i++) {
-        let gridCell = document.createElement("div");
-        gridCell.classList.add("grid-cell");
-        gridRow.appendChild(gridCell);
+function resetGame()
+{
+    document.getElementById("game-container").innerHTML = '';
+    do {
+        numCellsPerEdge = parseInt(prompt("How many squares per side? (Max 150)",numCellsPerEdge),10);
     }
-    
-    gameDiv.appendChild(gridRow);
+    while (!Number.isInteger(numCellsPerEdge) || numCellsPerEdge > 150);
+    for (let i = 0; i < numCellsPerEdge; i++) {
+        let gridRow = document.createElement("div");
+        gridRow.classList.add("grid-row")
+        for (let i = 0; i < numCellsPerEdge; i++) {
+            let gridCell = document.createElement("div");
+            gridCell.classList.add("grid-cell");
+            gridRow.appendChild(gridCell);
+        }
+
+        gameDiv.appendChild(gridRow);
+    }
+    let gridCells = Object.values(document.getElementsByClassName("grid-cell"));
+    gridCells.forEach( e => e.addEventListener("mouseover", ev => changeCellColor(ev)));
+}
+
+function changeCellColor(ev)
+{
+        ev.target.style.backgroundColor="green";
 }
